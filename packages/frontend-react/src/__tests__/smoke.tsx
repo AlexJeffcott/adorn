d5a29565d71db6353659df9ab9f75e8fd55ee026
ajt <alexjeffcott@gmail.com>
2022-05-04 18:01:49 +0200

    feat: enable Skypack

--- packages/frontend-react/src/__tests__/smoke.tsx
+++ packages/frontend-react/src/__tests__/smoke.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { expect } from 'chai';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('test smoke', () => {
 	it('smoke test', () => {
-		expect(render).to.eq(render);
+		render(<div>loading</div>);
+		expect(screen.getByText('loading')).to.exist;
 		expect(userEvent).to.eq(userEvent);
 	});
 });
